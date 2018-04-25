714c6cd0864e6900d5419ffae2361d2a0773584f
mayura <mayurauthpala@gmail.com>
2018-04-25 22:50:55 +0530

    Map Part done

--- app/src/main/java/com/example/mayur/pdm/EmMap.java
+++ app/src/main/java/com/example/mayur/pdm/EmMap.java
@@ -1,13 +1,57 @@
 package com.example.mayur.pdm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class EmMap extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class EmMap extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final int MY_REQUEST_INT = 177;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_em_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng malabe = new LatLng(-6.914664, 79.973001);
+        mMap.addMarker(new MarkerOptions().position(malabe).title("Marker in SLIIT"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(malabe));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    requestPermissions(new  String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION},MY_REQUEST_INT);
+                }
+            return;
+        }else{
+            mMap.setMyLocationEnabled(true);
+        }
+
+
+    }
+
+
 }
