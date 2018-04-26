d8422a9e619caf3dd6f83ab362cef90b5bed72bd
LENOVO <upadya.jayasekara@gmail.com>
2018-04-26 13:03:46 +0530

    new parts

--- app/src/main/java/com/example/mayur/pdm/HamidPage.java
+++ app/src/main/java/com/example/mayur/pdm/HamidPage.java
@@ -9,5 +9,7 @@ public class HamidPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hamid_page);
+
+        ///
     }
 }
