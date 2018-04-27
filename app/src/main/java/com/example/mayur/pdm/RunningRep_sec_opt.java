ef4c0ac8dd4edfe813cbeb8c2f36ae86c334f3d4
acer <inishaamanthi@gmail.com>
2018-04-27 12:18:52 +0530

    db insert

--- app/src/main/java/com/example/mayur/pdm/RunningRep_sec_opt.java
+++ app/src/main/java/com/example/mayur/pdm/RunningRep_sec_opt.java
@@ -2,15 +2,28 @@ package com.example.mayur.pdm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RunningRep_sec_opt extends AppCompatActivity {
 
     private Button btn10;
     private Button button9;
     private Button button8;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,12 @@ public class RunningRep_sec_opt extends AppCompatActivity {
         btn10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("booking");
+                HashMap userMap = new HashMap();
+                userMap.put("Section", "Running_Repair");
+                userMap.put("Type", "Wheel_alignment");
+                ref.push().setValue(userMap);
+
                 OpenBookDateTime();
             }
         });
@@ -29,21 +48,37 @@ public class RunningRep_sec_opt extends AppCompatActivity {
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("booking");
+                HashMap userMap = new HashMap();
+                userMap.put("Section", "Running_Repair");
+                userMap.put("Type", "Gereral_Repair");
+                ref.push().setValue(userMap);
+
                 OpenBookDateTime();
             }
         });
+
 
         button9= (Button) findViewById(R.id.button9);
         button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("booking");
+                HashMap userMap = new HashMap();
+                userMap.put("Section", "Running_Repair");
+                userMap.put("Type", "Fluid_transmission");
+                ref.push().setValue(userMap);
+
                 OpenBookDateTime();
             }
         });
+
     }
 
     public void OpenBookDateTime() {
         Intent intent = new Intent(this, BookDateTime.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/mayur/pdm/chooseService.java
+++ app/src/main/java/com/example/mayur/pdm/chooseService.java
@@ -2,15 +2,29 @@ package com.example.mayur.pdm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class chooseService extends AppCompatActivity {
 
     private Button service_section;
     private Button tuneup_section;
     private Button runrepbtn;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +35,34 @@ public class chooseService extends AppCompatActivity {
         service_section.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("booking");
+              //  ref.push().child("Section").setValue("Service_Section");
                 OpenSeviceSec();
             }
         });
+
 
         tuneup_section= (Button) findViewById(R.id.tuneup_section);
         tuneup_section.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("booking");
+               // ref.push().child("Section").setValue("Tuneup_Section");
                 openTuneupSec();
             }
         });
+
 
         runrepbtn= (Button) findViewById(R.id.runrepbtn);
         runrepbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("booking");
+              //  ref.push().child("Section").setValue("RunningRep_Section");
                 OpenRunRepSec();
             }
         });
+
     }
 
     public void OpenSeviceSec() {
@@ -56,4 +79,5 @@ public class chooseService extends AppCompatActivity {
         Intent intent = new Intent(this, RunningRep_sec_opt.class);
         startActivity(intent);
     }
-}
+
+    }
